0e0580fa42dd4769e08cc8f11c51b0d023df7eb6
Andrii Ovcharov <ovcharovcoder@gmail.com>
2026-04-09 23:18:53 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const { src, dest, watch, parallel, series } = gulp;
 const fs = require('fs');
+const path = require('path');
+
 const plugins = {
   scss: require('gulp-sass')(require('sass')),
   concat: require('gulp-concat'),
@@ -21,9 +23,14 @@ const plugins = {
   if: require('gulp-if'),
 };
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // File paths
 const paths = {
   imagesSrc: 'app/images/src/**/*.{jpg,jpeg,png,svg}',
+  imagesWebpSrc: 'app/images/src/**/*.{jpg,jpeg,png}',
+  imagesAvifSrc: 'app/images/src/**/*.{jpg,jpeg,png}',
+  imagesSvgSrc: 'app/images/src/**/*.svg',
   scriptsSrc: 'app/js/main.js',
   stylesSrc: 'app/scss/style.scss',
   htmlSrc: 'app/pages/*.html',
@@ -32,14 +39,14 @@ const paths = {
 
 // Processing HTML with components
 function pages() {
-  console.log('Обробка HTML...');
+  console.log('📄 Обробка HTML...');
   return src(paths.htmlSrc)
     .pipe(
       plugins.plumber({
         errorHandler: plugins.notify.onError(
-          'Error HTML: <%= error.message %>'
+          'Error HTML: <%= error.message %>',
         ),
-      })
+      }),
     )
     .pipe(plugins.include({ prefix: '@@', basepath: 'app/components/' }))
     .pipe(dest('app'))
@@ -48,165 +55,221 @@ function pages() {
 
 // Font optimization
 function fonts() {
-  return src(paths.fontsSrc)
+  console.log('🔤 Оптимізація шрифтів...');
+
+  if (!fs.existsSync('app/fonts')) {
+    fs.mkdirSync('app/fonts', { recursive: true });
+  }
+
+  const woffStream = src(paths.fontsSrc)
     .pipe(
       plugins.plumber({
         errorHandler: plugins.notify.onError(
-          'Error Fonts: <%= error.message %>'
+          'Error Fonts WOFF: <%= error.message %>',
         ),
-      })
+      }),
     )
-    .pipe(plugins.fonter({ formats: ['woff', 'ttf'] }))
-    .pipe(plugins.if(file => /\.woff$/.test(file.extname), dest('app/fonts')))
-    .pipe(src('app/fonts/src/*.ttf'))
+    .pipe(plugins.fonter({ formats: ['woff'] }))
+    .pipe(dest('app/fonts'));
+
+  const woff2Stream = src(paths.fontsSrc)
+    .pipe(
+      plugins.plumber({
+        errorHandler: plugins.notify.onError(
+          'Error Fonts WOFF2: <%= error.message %>',
+        ),
+      }),
+    )
     .pipe(plugins.ttf2woff2())
     .pipe(dest('app/fonts'));
+
+  return Promise.all([woffStream, woff2Stream]);
 }
 
-// Image optimization
-function images() {
-  return (
-    src(paths.imagesSrc)
-      .pipe(
-        plugins.plumber({
-          errorHandler: plugins.notify.onError(
-            'Error image: <%= error.message %>'
-          ),
-        })
-      )
-      .pipe(plugins.newer('app/images'))
-      // Copy SVG without changes
-      .pipe(plugins.if(file => /\.svg$/.test(file.extname), dest('app/images')))
-      // Convert JPG/PNG to AVIF
-      .pipe(
-        plugins.if(
-          file => /\.(jpg|jpeg|png)$/.test(file.extname),
-          plugins.avif({ quality: 50 })
-        )
-      )
-      .pipe(dest('app/images'))
-      // Reload for WebP
-      .pipe(src(paths.imagesSrc))
-      .pipe(plugins.newer('app/images'))
-      .pipe(
-        plugins.if(
-          file => /\.(jpg|jpeg|png)$/.test(file.extname),
-          plugins.webp()
-        )
-      )
-      .pipe(dest('app/images'))
-  );
+// Image conversion
+function imagesWebp() {
+  console.log('🖼️ Конвертація в WebP...');
+  return src(paths.imagesWebpSrc)
+    .pipe(
+      plugins.plumber({
+        errorHandler: plugins.notify.onError(
+          'Error WebP: <%= error.message %>',
+        ),
+      }),
+    )
+    .pipe(plugins.newer('app/images'))
+    .pipe(plugins.webp())
+    .pipe(dest('app/images'));
 }
+
+function imagesAvif() {
+  console.log('🖼️ Конвертація в AVIF...');
+  return src(paths.imagesAvifSrc)
+    .pipe(
+      plugins.plumber({
+        errorHandler: plugins.notify.onError(
+          'Error AVIF: <%= error.message %>',
+        ),
+      }),
+    )
+    .pipe(plugins.newer('app/images'))
+    .pipe(plugins.avif({ quality: 50 }))
+    .pipe(dest('app/images'));
+}
+
+function imagesSvg() {
+  console.log('🖼️ Копіювання SVG...');
+  return src(paths.imagesSvgSrc)
+    .pipe(
+      plugins.plumber({
+        errorHandler: plugins.notify.onError('Error SVG: <%= error.message %>'),
+      }),
+    )
+    .pipe(plugins.newer('app/images'))
+    .pipe(dest('app/images'));
+}
+
+const images = parallel(imagesWebp, imagesAvif, imagesSvg);
 
 // Script processing
 function scripts() {
+  console.log('📜 Обробка скриптів...');
+
   return src(paths.scriptsSrc)
     .pipe(
       plugins.plumber({
         errorHandler: plugins.notify.onError(
-          'Error scripts: <%= error.message %>'
+          'Error scripts: <%= error.message %>',
         ),
-      })
+      }),
     )
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.concat('main.min.js'))
     .pipe(plugins.uglify())
-    .pipe(plugins.sourcemaps.write())
+    .pipe(plugins.sourcemaps.write('.'))
     .pipe(dest('app/js'))
     .pipe(plugins.browserSync.reload({ stream: true }));
 }
 
 // Style processing
 function styles() {
-  fs.mkdirSync('app/css', { recursive: true });
+  console.log('🎨 Обробка стилів...');
+
+  if (!fs.existsSync('app/css')) {
+    fs.mkdirSync('app/css', { recursive: true });
+  }
+
   return src(paths.stylesSrc)
     .pipe(
       plugins.plumber({
         errorHandler: plugins.notify.onError(
-          'Error styles: <%= error.message %>'
+          'Error styles: <%= error.message %>',
         ),
-      })
+      }),
     )
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.scss({ outputStyle: 'compressed' }))
     .pipe(plugins.concat('style.min.css'))
     .pipe(plugins.postcss([require('autoprefixer')]))
-    .pipe(plugins.sourcemaps.write())
+    .pipe(plugins.sourcemaps.write('.'))
     .pipe(dest('app/css'))
     .pipe(plugins.browserSync.stream());
 }
 
 // Browser synchronization
-function sync() {
+function sync(done) {
   plugins.browserSync.init({
     server: { baseDir: 'app/' },
     notify: false,
     port: 3000,
     ghostMode: false,
     online: true,
+    open: true,
   });
+  done();
 }
 
 // Watching
 function watching() {
-  console.log('👀 File watching...');
-  sync();
+  console.log('👀 Початок спостереження за файлами...');
+  console.log('🌐 Сервер запущено: http://localhost:3000');
 
-  // SCSS watching
   watch('app/scss/**/*.scss', styles);
-
-  // HTML watching
   watch(['app/components/**/*.html', 'app/pages/*.html'], pages);
-
-  // JS watching
   watch('app/js/main.js', scripts);
-
-  // Images watching
   watch(
     paths.imagesSrc,
-    series(images, function (cb) {
+    series(images, function (done) {
       plugins.browserSync.reload();
-      cb();
-    })
+      done();
+    }),
   );
-
-  // Fonts watching
   watch(paths.fontsSrc, fonts);
 }
 
 // Clean up the dist folder
 function cleanDist() {
-  return src('dist', { allowEmpty: true }).pipe(plugins.clean());
+  console.log('🧹 Очищення папки dist...');
+  return src('dist', { allowEmpty: true, read: false }).pipe(plugins.clean());
 }
 
 // Build for production
 function building() {
+  console.log('🏗️ Збірка для production...');
   return src(
     [
-      'app/css/style.min.css',
+      'app/css/**/*.css',
       'app/images/**/*.{svg,webp,avif}',
-      'app/fonts/*.{woff,woff2}',
+      'app/fonts/**/*.{woff,woff2}',
       'app/js/main.min.js',
       'app/*.html',
     ],
-    { base: 'app', allowEmpty: true }
-  ).pipe(dest('dist'));
+    { base: 'app', allowEmpty: true },
+  )
+    .pipe(
+      plugins.if(
+        isProd,
+        plugins.replace(/(src|href)=["']\/([^"']+)/g, '$1="$2'),
+      ),
+    )
+    .pipe(
+      plugins.if(isProd, plugins.replace(/url\(['"]?\/([^)'"]+)/g, 'url($1)')),
+    )
+    .pipe(
+      plugins.if(
+        isProd,
+        plugins.replace(/<img([^>]+)src=["']\/([^"']+)/g, '<img$1src="$2'),
+      ),
+    )
+    .pipe(dest('dist'));
 }
 
+// Export of tasks
 exports.styles = styles;
+exports.imagesWebp = imagesWebp;
+exports.imagesAvif = imagesAvif;
+exports.imagesSvg = imagesSvg;
 exports.images = images;
 exports.fonts = fonts;
 exports.pages = pages;
 exports.scripts = scripts;
-exports.watching = watching;
+exports.watch = watching;
 exports.cleanDist = cleanDist;
+
 exports.build = series(
   cleanDist,
-  images,
-  fonts,
+  parallel(images, fonts),
   styles,
   scripts,
   pages,
-  building
+  building,
 );
-exports.default = series(images, fonts, styles, scripts, pages, watching);
+
+exports.default = series(
+  parallel(images, fonts),
+  styles,
+  scripts,
+  pages,
+  sync,
+  watching,
+);
